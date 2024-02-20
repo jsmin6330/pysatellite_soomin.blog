f824d4ba9da7bc061050d86efa7d3fd2a5fe66ba
jsmin6330 <jsmin6330@gmail.com>
2024-02-20 12:32:25 +0900

    update manual-compose.sh

--- manual-compose.sh
+++ manual-compose.sh
@@ -2,12 +2,16 @@
 
 # rm & rmi
 # figlet remove all
-figlet remove blog-a , blog-b
+figlet remove blog-a , blog-b, lb
 sudo docker stop $(sudo docker ps -q)
 # sudo docker rm $(sudo docker ps -a -q) # 전체 컨테이너 삭제
-sudo docker rm blog-a blog-b
+sudo docker rm blog-a blog-b lb
 # sudo docker rmi $(sudo docker images -q) # 전체 이미지 삭제
-sudo docker rmi blog-a blog-b
+sudo docker rmi blog-a blog-b lb
+
+# network remove
+figlet network remove blogab-lb
+sudo docker network rm blogab-lb
 
 # build
 figlet build blog-a , blog-b
@@ -18,11 +22,6 @@ sudo docker build -t blog-b -f docker/blog-b/Dockerfile docker/blog-b
 figlet run blog-a , blog-b
 sudo docker run -d --name blog-a -p 8001:80 blog-a
 sudo docker run -d --name blog-b -p 8002:80 blog-b
-
-
-# network remove
-figlet network remove blogab-lb
-sudo docker network rm blogab-lb
 
 # network
 figlet network blogab-lb
@@ -38,7 +37,6 @@ sudo docker build -t lb -f docker/lb/Dockerfile docker/lb
 # run
 figlet run lb
 sudo docker run -d --name lb -p 8003:80 lb
-sudo docker start lb
 
 # ps
 figlet ps
@@ -46,6 +44,7 @@ sudo docker ps
 
 # network
 figlet network lb
+sudo docker start lb
 sudo docker network connect blogab-lb lb
 sudo docker network inspect blogab-lb
 
